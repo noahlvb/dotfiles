de7cc02f88a76a84cd0d0249f17c880cb0488dd1
Noah van Bochove <noahlvb@gmail.com>
2017-11-07 19:11:18 +0100

    Added mongodb to the packages

--- init/apt.sh
+++ init/apt.sh
@@ -13,6 +13,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo add-apt-repository "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main"
 sudo add-apt-repository "deb http://repository.spotify.com stable non-free"
+sudo add-apt-repository "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse"
 sudo curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
 
 # Updating repo's
@@ -41,6 +42,7 @@ packages=(
     teamviewer
     skypeforlinux
     plymouth
+    mongodb-org
 #   -- Python --
     python3-xmltodict
     python3-flask
